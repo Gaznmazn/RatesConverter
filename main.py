173e22b0da80f8b0e7c9efda3d327de63b0486bd
Gaznmazn <misaser765@gmail.com>
2026-06-30 19:15:39 +0300

    feat: integrate UI layout into main.py and update gitignore

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import sys
+import json
+from ui_converter import UiMainWindow
 from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout,
                              QHBoxLayout, QComboBox, QDoubleSpinBox, QMenu)
-import json
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self._setup_ui()
+        self.ui = UiMainWindow()
+        self.ui.setupUi(self)
 
     def _setup_ui(self):
         pass
