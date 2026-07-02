41e6d947b2de40ec8d1e2904f1d7a9ba0ced9ee6
Gaznmazn <misaser765@gmail.com>
2026-07-02 15:56:50 +0300

    refactor: apply architectural layers, update variable names and fix spinbox limits

--- main.py
+++ main.py
@@ -14,35 +14,43 @@ class MainWindow(QWidget):
     def _setup_ui(self):
         self.ui = UiMainWindow()
         self.ui.setupUi(self)
+        self.ui.input_amount.setMinimum(0.0)
+        self.ui.input_amount.setMaximum(9999999999999999999999999999999999999999999999.0)
 
     def _bind_signals(self):
-        self.list_of_country()
+        self._data_layer()
         self.ui.btn_swap.clicked.connect(self.click_on_reverse)
-        self.ui.combo_from.currentTextChanged.connect(self.count_course)
-        self.ui.combo_to.currentTextChanged.connect(self.count_course)
-        self.ui.input_amount.valueChanged.connect(self.count_course)
-        self.count_course()
+        self.ui.combo_from.currentTextChanged.connect(self._logic)
+        self.ui.combo_to.currentTextChanged.connect(self._logic)
+        self.ui.input_amount.valueChanged.connect(self._logic)
+        self._logic()
 
     def closeEvent(self, event):
-        pass
+        event.accept()
 
-    def list_of_country(self):
+    def _data_layer(self):
         with open('data/rates.json') as data:
             self.rates = json.load(data)['rates']
-            self.ui.combo_to.addItems([val for val in self.rates.keys()])
-            self.ui.combo_from.addItems([val for val in self.rates.keys()])
+        self.ui.combo_to.addItems([val for val in self.rates.keys()])
+        self.ui.combo_from.addItems([val for val in self.rates.keys()])
 
     def click_on_reverse(self):
         word = self.ui.combo_from.currentText()
         self.ui.combo_from.setCurrentText(self.ui.combo_to.currentText())
         self.ui.combo_to.setCurrentText(word)
 
-    def count_course(self):
-        course_from = self.ui.combo_from.currentText()
-        course_to = self.ui.combo_to.currentText()
-        input_count = self.ui.input_amount.value()
-        result = f'{(input_count / self.rates[course_from]) * self.rates[course_to]:.2f}'.replace('.', ',')
-        self.ui.label_result.setText(result)
+    def _logic(self):
+        image_path = ''
+        currency_from = self.ui.combo_from.currentText()
+        currency_to = self.ui.combo_to.currentText()
+        amount = self.ui.input_amount.value()
+        rate = (1 / self.rates[currency_from]) * self.rates[currency_to]
+
+        if amount >= 0 and rate > 0:
+            result = amount * rate
+            self.ui.label_result.setText(f'{result:.2f}'.replace('.', ','))
+        else:
+            self.ui.label_result.setText("0,00")
 
 
 if __name__ == '__main__':
